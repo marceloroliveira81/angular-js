924e78733962be540777d23e8fe0c5475b20d919
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-07-10 22:37:26 -0500

    Acesso ao Webservice
    Requisição com $http para buscar uma lista de objetos em formato json.

--- controllers/controllerMain.js
+++ controllers/controllerMain.js
@@ -1,5 +1,14 @@
 angular.module('main')
-.controller('controllerMain', function($scope){
+.controller('controllerMain', function($scope, $http){
 	$scope.titulo = 'Angular - Bootstrap';
 	$scope.mensagem = 'AngularJS - Bootstrap';
+
+	$scope.tituloPainel = 'Lista de Pessoas';
+	$scope.pessoas = {};
+
+	$http.get('http://localhost/webservice-angular-js/lista-pessoas.json')
+	.then(function(response){
+		$scope.pessoas = response.data;
+	});
+
 });
