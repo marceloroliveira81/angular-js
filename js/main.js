7f3cd4a11c9990502e7aa626e9382a094bfb7241
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-07-19 22:45:20 -0500

    Criação da rotas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-angular.module('main', ['minhasDirectivas']);
+angular.module('main', ['minhasDirectivas', 'ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+
+	$routeProvider
+	
+	.when('/principal', {
+		templateUrl : 'pages/principal.html',
+		controller : 'controllerMain'
+	})
+	
+	.otherwise({
+		template : '<h1>Rota Padrão</h1>'
+	});
+
+	$locationProvider.hashPrefix('');
+	$locationProvider.html5Mode(true);
+	
+});
